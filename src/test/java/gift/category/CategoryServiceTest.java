6bed323cf5d30316e1fe5b70f4d2a130e1e0049b
songsimo <chy.song89@gmail.com>
2026-06-01 15:51:10 +0900

    refactor(category): delegate getCategories to CategoryService

---
+++ src/test/java/gift/category/CategoryServiceTest.java
@@ -0,0 +1,33 @@
+package gift.category;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class CategoryServiceTest {
+
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryService categoryService;
+
+    @Test
+    void getAll_카테고리_목록을_반환한다() {
+        given(categoryRepository.findAll())
+            .willReturn(List.of(new Category("식품", "#fff", "img.png", "desc")));
+
+        List<CategoryResponse> result = categoryService.getAll();
+
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).name()).isEqualTo("식품");
+    }
+}
